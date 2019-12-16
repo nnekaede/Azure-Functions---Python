e4dfb7a7f2385ffef143cbd11784e65f4a5a99b1
Nneka Ede <nneka.ede@chevron.com>
2019-12-16 16:27:47 -0600

    added faulty response

--- dog-classfication/__init__.py
+++ dog-classfication/__init__.py
@@ -19,4 +19,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Scoring step
     results = predict.predict_url(image_url)
 
-    return json.dumps(results)
+    if name:
+        return json.dumps(results)
+    else:
+        return func.HttpResponse(
+             "Please pass a link on the query string or in the request body",
+             status_code=400
+        )
