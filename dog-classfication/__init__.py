be2ffb393251bd24db8d00ea51d8c9259483f51f
Nneka Ede <nneka.ede@chevron.com>
2019-12-16 22:27:48 -0600

    added input params

--- dog-classfication/__init__.py
+++ dog-classfication/__init__.py
@@ -20,8 +20,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     from . import predict
     image_url = req.params.get('img')
+    if not image_url:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            image_url = req_body.get('img')
 
-    if image_url == "" :
+    if not image_url:
         return func.HttpResponse(
             "Please pass a link on the query string or in the request body",
             status_code=400
